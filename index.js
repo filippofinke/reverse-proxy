9fc58767e770b9ec4513d958ae4301d81aa3576e
filippofinke <filippofinke@protonmail.ch>
2021-07-20 13:57:10 +0000

    :bug: Fix connection timeout

--- index.js
+++ index.js
@@ -15,20 +15,31 @@ const config = require("./config.js");
 
 const proxy = httpProxy.createProxyServer({});
 
+proxy.on("proxyReq", (proxyReq, req, res, options) => {
+	let timeout = setTimeout(() => proxyReq.destroy(), config.connectionTimeout);
+
+	proxyReq.on("connect", () => {
+		clearTimeout(timeout);
+	});
+});
+
 const server = http.createServer((req, res) => {
 	let hostname = req.headers.host;
 
 	if (config.services[hostname]) {
 		let target = config.services[hostname];
 		log(`${hostname} -> ${target}`);
-		return proxy.web(req, res, { target }, (err, req, res) => {
-			log(`${hostname} -> is offline!`);
-			return res.end(`The requested service is not responding.`);
+		proxy.web(req, res, { target }, (err, req, res) => {
+			res.writeHead(501, { "Content-Type": "text/plain" });
+			res.write("The requested service is unavailable.");
+			res.end();
 		});
+	} else {
+		log(`${hostname} -> not found!`);
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.write("Service not found.");
+		res.end();
 	}
-
-	log(`${hostname} -> not found!`);
-	return res.end("Service not found");
 });
 
 server.listen(config.port, () => {
