5dbe1cc5a0fbbf82207e29784c51ece81e8b66e0
filippofinke <filippofinke@protonmail.ch>
2021-08-27 19:37:10 +0000

    🔥 Remove console.log

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const fetch = require("node-fetch");
 const ipRangeCheck = require("ip-range-check");
 
 const log = (text) => {
-  let date = new Date();
-  console.log(date.toISOString() + " | " + text);
+	let date = new Date();
+	console.log(date.toISOString() + " | " + text);
 };
 
 if (!fs.existsSync("config.js")) {
-  log("ERROR! Please copy your config.sample.js to config.js");
-  process.exit(0);
+	log("ERROR! Please copy your config.sample.js to config.js");
+	process.exit(0);
 }
 const config = require("./config.js");
 
@@ -20,73 +20,70 @@ const proxy = httpProxy.createProxyServer({});
 let cloudflareIps = [];
 
 fetch("https://www.cloudflare.com/ips-v4")
-  .then((r) => r.text())
-  .then((text) => {
-    cloudflareIps = text.split("\n");
-  });
+	.then((r) => r.text())
+	.then((text) => {
+		cloudflareIps = text.split("\n");
+	});
 
 proxy.on("proxyReq", (proxyReq, req, res, options) => {
-  let timeout = setTimeout(() => proxyReq.destroy(), options.timeout);
+	let timeout = setTimeout(() => proxyReq.destroy(), options.timeout);
 
-  proxyReq.on("connect", () => {
-    clearTimeout(timeout);
-  });
+	proxyReq.on("connect", () => {
+		clearTimeout(timeout);
+	});
 });
 
 const server = http.createServer((req, res) => {
-  let hostname = req.headers.host;
+	let hostname = req.headers.host;
 
-  delete req.headers["x-forwarded-for"];
-  delete req.headers["x-forwarded-host"];
-  delete req.headers["x-forwarded-proto"];
+	delete req.headers["x-forwarded-for"];
+	delete req.headers["x-forwarded-host"];
+	delete req.headers["x-forwarded-proto"];
 
-  if (
-    ipRangeCheck(req.socket.remoteAddress, cloudflareIps) &&
-    req.headers["cf-connecting-ip"]
-  ) {
-    req.headers["x-forwarded-for"] = req.headers["cf-connecting-ip"];
-  } else {
-    req.headers["x-forwarded-for"] = req.socket.remoteAddress;
-  }
+	if (
+		ipRangeCheck(req.socket.remoteAddress, cloudflareIps) &&
+		req.headers["cf-connecting-ip"]
+	) {
+		req.headers["x-forwarded-for"] = req.headers["cf-connecting-ip"];
+	} else {
+		req.headers["x-forwarded-for"] = req.socket.remoteAddress;
+	}
 
-  let service = config.services.find((s) => {
-    if (typeof s.hostname === "string") {
-      s.hostname = [s.hostname];
-    }
+	let service = config.services.find((s) => {
+		if (typeof s.hostname === "string") {
+			s.hostname = [s.hostname];
+		}
 
-    for (let host of s.hostname) {
-      console.log(hostname, host);
-      if (hostname == host || (s.endsWith && hostname.endsWith(host))) {
-        return true;
-      }
-    }
+		for (let host of s.hostname) {
+			if (hostname == host || (s.endsWith && hostname.endsWith(host))) {
+				return true;
+			}
+		}
 
-    return false;
-  });
+		return false;
+	});
 
-  console.log(service);
-
-  if (service) {
-    let target = service.target;
-    log(`${hostname} -> ${target}`);
-    proxy.web(
-      req,
-      res,
-      { target, timeout: service.timeout || config.connectionTimeout },
-      (err, req, res) => {
-        res.writeHead(503, { "Content-Type": "text/plain" });
-        res.write("The requested service is unavailable.");
-        res.end();
-      }
-    );
-  } else {
-    log(`${hostname} -> not found!`);
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.write("Service not found.");
-    res.end();
-  }
+	if (service) {
+		let target = service.target;
+		log(`${hostname} -> ${target}`);
+		proxy.web(
+			req,
+			res,
+			{ target, timeout: service.timeout || config.connectionTimeout },
+			(err, req, res) => {
+				res.writeHead(503, { "Content-Type": "text/plain" });
+				res.write("The requested service is unavailable.");
+				res.end();
+			}
+		);
+	} else {
+		log(`${hostname} -> not found!`);
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.write("Service not found.");
+		res.end();
+	}
 });
 
 server.listen(config.port, "0.0.0.0", () => {
-  console.log(`reverse-proxy listening on port ${config.port}`);
+	console.log(`reverse-proxy listening on port ${config.port}`);
 });
