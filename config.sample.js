48bb00b0a2873f4eab2c1a0167273ab0f4e7768f
filippofinke <filippofinke@protonmail.ch>
2021-08-27 15:49:34 +0200

    ✨ Add support to multiple hostnames

--- config.sample.js
+++ config.sample.js
@@ -1,4 +1,15 @@
 module.exports = {
-	port: 8080,
-	services: [{ hostname: "test.com", target: "http://127.0.0.1:3000" }],
+  port: 8080,
+  services: [
+    {
+      hostname: "test.com",
+      target: "http://127.0.0.1:3000",
+      timeout: 5000,
+      endsWith: false,
+    },
+    {
+      hostname: ["127.0.0.1", "localhost"],
+      target: "http://127.0.0.1:3000",
+    },
+  ],
 };
