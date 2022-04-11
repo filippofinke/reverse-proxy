56bb43a8e750d5d4d54a860787f206dfa40f0440
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-04-11 18:34:48 +0200

    feat: :sparkles: add optional authentication

--- config.sample.js
+++ config.sample.js
@@ -2,14 +2,20 @@ module.exports = {
   port: 8080,
   services: [
     {
-      hostname: "test.com",
-      target: "http://127.0.0.1:3000",
+      hostname: "localhost:8080",
+      target: "http://127.0.0.1:8081",
       timeout: 5000,
       endsWith: false,
+      auth:{
+        username: "admin",
+        password: "admin"
+      }
     },
     {
-      hostname: ["127.0.0.1", "localhost"],
-      target: "http://127.0.0.1:3000",
-    },
-  ],
+      hostname: "127.0.0.1:8080",
+      target: "http://127.0.0.1:8081",
+      timeout: 5000,
+      endsWith: false
+    }
+  ]
 };
